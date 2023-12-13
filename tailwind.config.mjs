fc60eeafb643292634996faafa32d7ed3ada0685
Parker <parkied@gmail.com>
2023-12-12 17:26:18 -0700

    Add color tokens

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,18 @@
 export default {
     content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
     theme: {
-        extend: {},
+        extend: {
+            colors: {
+                Blue: '#74acff',
+                Green: '#a5f91d',
+                Orange: '#ff7a1a',
+                Purple: '#747fef',
+                Sand: '#fffbf1',
+                Yellow: '#ffd400',
+                Black: '#121212',
+                'Black-30': '#1212124d',
+            },
+        },
     },
     plugins: [require('@tailwindcss/typography')],
 };

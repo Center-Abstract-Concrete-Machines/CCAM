c18927a9424e6e5e164e2665fc5c4438c3ba4efb
Parker <parkied@gmail.com>
2024-02-16 15:09:56 -0700

    Update filters

--- src/utils/programFilters.js
+++ src/utils/programFilters.js
@@ -1,26 +1,30 @@
-import dayjs from "dayjs";
-import isSameOrBefore from 'dayjs/plugin/isSameOrBefore'
+import dayjs from 'dayjs';
+import isSameOrBefore from 'dayjs/plugin/isSameOrBefore';
+import isSameOrAfter from 'dayjs/plugin/isSameOrAfter';
 import utc from 'dayjs/plugin/utc';
-import timezone from 'dayjs/plugin/timezone'
+import timezone from 'dayjs/plugin/timezone';
 
 dayjs.extend(isSameOrBefore);
+dayjs.extend(isSameOrAfter);
 dayjs.extend(utc);
 dayjs.extend(timezone);
 
 export function isDateUpcoming(date) {
     const today = dayjs(new Date());
-    const eventDate = dayjs.utc(date).tz('America/Chicago', true).endOf('day')
+    const eventDate = dayjs.utc(date).tz('America/Chicago', true).endOf('day');
     return today.isSameOrBefore(eventDate, 'date');
 }
 
 export function isEventUpcoming(eventData) {
     if (eventData.endDate instanceof Date) {
-        return isDateUpcoming(eventData.endDate)
+        return isDateUpcoming(eventData.endDate);
     } else return true; // Such as if endDate is string 'ongoing'
 }
 
 export function isAfterPubDate(eventData) {
-    return eventData.pubDate < Date.now();
+    const today = dayjs(new Date());
+    const pubDate = dayjs.utc(eventData.pubDate).tz('America/Chicago', true);
+    return today.isSameOrAfter(pubDate, 'date');
 }
 
 export function sortByPubDate(a, b) {
--- src/utils/programFilters.test.js
+++ src/utils/programFilters.test.js
@@ -1,46 +1,82 @@
-import { test, expect, vi, describe, beforeEach, afterEach, useFakeTimers, userRealTimers } from 'vitest'
+import { test, expect, vi, describe, beforeEach, afterEach } from 'vitest';
 
-import { isEventUpcoming, isDateUpcoming } from './programFilters'
+import { isEventUpcoming, isDateUpcoming } from './programFilters';
 
 describe('isEventUpcoming works', () => {
     beforeEach(() => {
-        vi.useFakeTimers()
-    })
+        vi.useFakeTimers();
+    });
     afterEach(() => {
-        vi.useRealTimers()
-    })
+        vi.useRealTimers();
+    });
 
-    
-    test('date', () => {
-        vi.setSystemTime(new Date('2024-02-15'))
-        expect(isEventUpcoming({ endDate: new Date('2024-02-15')})).toBe(true)
+    test('same date passes', () => {
+        vi.setSystemTime(new Date('2024-02-15'));
+        expect(isEventUpcoming({ endDate: new Date('2024-02-15') })).toBe(true);
 
-        vi.setSystemTime(new Date('2024-02-15T01:01:00'))
-        expect(isEventUpcoming({ endDate: new Date('2024-02-15')})).toBe(true)
+        vi.setSystemTime(new Date('2024-02-15T00:00:00'));
+        expect(isEventUpcoming({ endDate: new Date('2024-02-15') })).toBe(true);
 
-        vi.setSystemTime(new Date('2024-02-15T23:24:00'))
-        expect(isEventUpcoming({ endDate: new Date('2024-02-15')})).toBe(true)
-    })
+        vi.setSystemTime(new Date('2024-02-15T23:59:59'));
+        expect(isEventUpcoming({ endDate: new Date('2024-02-15') })).toBe(true);
+    });
 
-})
+    test('later date does not pass', () => {
+        vi.setSystemTime(new Date('2024-02-16T00:00:00'));
+        expect(isEventUpcoming({ endDate: new Date('2024-02-15') })).toBe(
+            false
+        );
+
+        vi.setSystemTime(new Date('2024-02-16T12:00:00'));
+        expect(isEventUpcoming({ endDate: new Date('2024-02-15') })).toBe(
+            false
+        );
+
+        vi.setSystemTime(new Date('2024-02-16T23:59:59'));
+        expect(isEventUpcoming({ endDate: new Date('2024-02-15') })).toBe(
+            false
+        );
+    });
+});
 
 describe('isDateUpcoming works', () => {
     beforeEach(() => {
-        vi.useFakeTimers()
-    })
+        vi.useFakeTimers();
+    });
     afterEach(() => {
-        vi.useRealTimers()
-    })
+        vi.useRealTimers();
+    });
 
-    
-    test('date', () => {
-        vi.setSystemTime(new Date('2024-02-15'))
-        expect(isDateUpcoming(new Date('2024-02-15'))).toBe(true)
+    test('same date passes', () => {
+        vi.setSystemTime(new Date('2024-02-15'));
+        expect(isDateUpcoming(new Date('2024-02-15'))).toBe(true);
 
-        vi.setSystemTime(new Date('2024-02-15T01:01:00'))
-        expect(isDateUpcoming(new Date('2024-02-15'))).toBe(true)
+        vi.setSystemTime(new Date('2024-02-15T00:00:00'));
+        expect(isDateUpcoming(new Date('2024-02-15'))).toBe(true);
 
-        vi.setSystemTime(new Date('2024-02-15T23:24:00'))
-        expect(isDateUpcoming(new Date('2024-02-15'))).toBe(true)
-    })
-})
+        vi.setSystemTime(new Date('2024-02-15T23:59:59'));
+        expect(isDateUpcoming(new Date('2024-02-15'))).toBe(true);
+    });
+
+    test('later date does not pass', () => {
+        vi.setSystemTime(new Date('2024-02-16T00:00:00'));
+        expect(isDateUpcoming(new Date('2024-02-15'))).toBe(false);
+
+        vi.setSystemTime(new Date('2024-02-16T12:00:00'));
+        expect(isDateUpcoming(new Date('2024-02-15'))).toBe(false);
+
+        vi.setSystemTime(new Date('2024-02-16T23:59:59'));
+        expect(isDateUpcoming(new Date('2024-02-15'))).toBe(false);
+    });
+
+    test('earlier date does pass', () => {
+        vi.setSystemTime(new Date('2024-02-14T00:00:00'));
+        expect(isDateUpcoming(new Date('2024-02-15'))).toBe(true);
+
+        vi.setSystemTime(new Date('2024-02-14T12:00:00'));
+        expect(isDateUpcoming(new Date('2024-02-15'))).toBe(true);
+
+        vi.setSystemTime(new Date('2024-02-14T23:59:59'));
+        expect(isDateUpcoming(new Date('2024-02-15'))).toBe(true);
+    });
+});
